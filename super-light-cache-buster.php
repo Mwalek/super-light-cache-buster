1fc6346af23656d5c1eebe2eb0cde327685c29cf
Mwale Kalenga <mwale.kalenga@prosites.space>
2022-11-06 13:22:54 +0200

    Add 'text domain' and 'domain path' to plugin header.

--- super-light-cache-buster.php
+++ super-light-cache-buster.php
@@ -13,6 +13,8 @@
  * Author URI:        https://mwale.me
  * License:           GPL-3.0+
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.en.html
+ * Text Domain:       super-light-cache-buster
+ * Domain Path:       /languages
  */
 
 // Exit if accessed directly.
